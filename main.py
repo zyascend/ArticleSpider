00d62ac9501152707171a899353e670fcdeecc52
张扬 <1334553391@qq.com>
2017-11-05 22:59:19 +0800

    day2

---
+++ main.py
@@ -0,0 +1,8 @@
+# -*-coding:utf-8-*-
+import os
+from scrapy.cmdline import execute
+import sys
+
+main_path = os.path.dirname(os.path.abspath(__file__)) # 获取工程目录
+sys.path.append(main_path)
+execute(['scrapy', 'crawl', 'jobbole'])
