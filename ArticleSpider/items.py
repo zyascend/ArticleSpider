9388be8f8743ac68ee0178b00a7554c8406691e9
张扬 <1334553391@qq.com>
2017-11-12 22:25:17 +0800

    添加zhihu.py

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -77,3 +77,27 @@ class JobBoleArticleItem(scrapy.Item):
 
     )
     content = scrapy.Field()
+
+class ZhihuQuestionItem(scrapy.Item):
+    zhihu_id = scrapy.Field()
+    topics = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    content = scrapy.Field()
+    answer_num = scrapy.Field()
+    comment_num = scrapy.Field()
+    watch_num = scrapy.Field()
+    click_num = scrapy.Field()
+    crawl_time = scrapy.Field()
+
+class ZhihuAnswerItem(scrapy.Item):
+    zhihu_id = scrapy.Field()
+    url = scrapy.Field()
+    question_id = scrapy.Field()
+    author_id = scrapy.Field()
+    content = scrapy.Field()
+    praise_num = scrapy.Field()
+    comment_num = scrapy.Field()
+    crawl_time = scrapy.Field()
+    create_time = scrapy.Field()
+    update_time = scrapy.Field()
